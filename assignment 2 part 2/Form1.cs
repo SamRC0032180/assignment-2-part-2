000ad533ffdff36c8595ecd489d33b8d1654b122
RC0032180 <RC0032180@CRO-24-04>
2019-04-16 10:52:16 +0100

    Add project files.

---
+++ assignment 2 part 2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment_2_part_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+
+                double resistor1 = double.Parse(textBox1.Text);
+            double resistor2 = double.Parse(textBox2.Text);
+            double resistor3 = 0.0;
+            if (textBox3.Text == "0")
+                resistor3 = double.PositiveInfinity;
+            else
+                resistor3 = double.Parse(textBox3.Text);
+
+            double resistor4 = 0.0;
+            if (textBox4.Text == "0")
+                resistor1 = double.PositiveInfinity;
+            else
+                resistor3 = double.Parse(textBox4.Text);
+
+
+
+            }
+}
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
